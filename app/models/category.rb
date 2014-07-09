a7b28414500d23889536412b2dab8fce17b04347
fadawar <fadawar@gmail.com>
2014-07-09 11:06:32 +0200

    Pridane parsovanie kategorie.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,16 @@
 class Category < ActiveRecord::Base
   has_many :articles
 
+  def self.parse_categories(html)
+    doc = Nokogiri::HTML.parse(html)
+
+    mainmenu = doc.search('.mainmenu li.active a').text
+    sub_item = doc.search('.submenu li.active a').first
+    submenu = sub_item.text
+    cat_name = mainmenu + ': ' + submenu
+    submenu_url = sub_item[:href]
+
+    Category.find_or_create_by!(name: cat_name, url: submenu_url)
+  end
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'parse category from article' do
+    html = File.read(File.dirname(__FILE__) + '/fixtures/article-page.html', encoding: 'utf-8')
+
+    Category.parse_categories(html)
+    expect(Category.all.size).to eq(1)
+
+    category = Category.first
+    expect(category.name).to eq('Relax: Cestovanie')
+    expect(category.url).to eq('http://relax.etrend.sk/relax-cestovanie.html')
+  end
 end
